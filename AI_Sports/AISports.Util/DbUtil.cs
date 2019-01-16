68e0f9c11d75926cfe808e9b820a0116d103b281
cqz <305865088@qq.com>
2019-01-16 17:47:54 +0800

    添加DbUtil

---
+++ AI_Sports/AISports.Util/DbUtil.cs
@@ -0,0 +1,57 @@
+﻿
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using AISports.Util;
+
+namespace AI_Sports.Util
+{
+
+
+   public class DbUtil
+    {
+
+        private static string DbName;
+        private static string DbUserName;
+        private static string DbPassword;
+        private static string DbUrl;
+        private static string connstr;
+
+        private DbUtil()
+        {
+
+        }
+        /// <summary>
+        /// 静态代码块初始化
+        /// </summary>
+        static DbUtil()
+        {
+            
+            DbName = "ai_sports";
+            DbUserName = "root";  
+            DbPassword = "root";
+            DbUrl = "127.0.0.1";
+            DbName = ConfigUtil.Get("DbName");
+            DbUserName = ConfigUtil.Get("DbUserName");  
+            DbPassword = ConfigUtil.Get("DbPassword");
+            DbUrl = ConfigUtil.Get("DbUrl");
+
+            connstr = string.Format("server={0};user id={1}; password={2}; database={3}; pooling=true;Charset=utf8", DbUrl, DbUserName, DbPassword, DbName);
+        }
+ 
+        public static MySqlConnection getConn()
+        {
+            return new MySqlConnection(connstr);
+        }
+
+
+
+    }
+
+
+}
