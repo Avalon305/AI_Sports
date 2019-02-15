3a6fe51ba867fa0ede5557e9223358e9e97f9aa6
mueller <321892803@qq.com>
2019-02-15 21:49:57 +0800

    再次测试

--- AI_Sports/NolleWindow.xaml.cs
+++ AI_Sports/NolleWindow.xaml.cs
@@ -26,6 +26,71 @@ namespace AI_Sports
     {
         TrainingDeviceRecordService trainingDeviceRecordService = new TrainingDeviceRecordService();
         DatacodeDAO datacodeDAO = new DatacodeDAO();
+        List<string> list0CF = new List<string>();
+        List<string> list0RF = new List<string>();
+        List<string> list0Rate = new List<string>();
+
+        List<string> list1CF = new List<string>();
+        List<string> list1RF = new List<string>();
+        List<string> list1Rate = new List<string>();
+
+        List<string> list2CF = new List<string>();
+        List<string> list2RF = new List<string>();
+        List<string> list2Rate = new List<string>();
+
+        List<string> list3CF = new List<string>();
+        List<string> list3RF = new List<string>();
+        List<string> list3Rate = new List<string>();
+
+        List<string> list4CF = new List<string>();
+        List<string> list4RF = new List<string>();
+        List<string> list4Rate = new List<string>();
+
+        List<string> list5CF = new List<string>();
+        List<string> list5RF = new List<string>();
+        List<string> list5Rate = new List<string>();
+    
+        List<string> list6CF = new List<string>();
+        List<string> list6RF = new List<string>();
+        List<string> list6Rate = new List<string>();
+
+        List<string> list7CF = new List<string>();
+        List<string> list7RF = new List<string>();
+        List<string> list7Rate = new List<string>();
+
+        List<string> list8CF = new List<string>();
+        List<string> list8RF = new List<string>();
+        List<string> list8Rate = new List<string>();
+
+
+        List<string> list9CF = new List<string>();
+        List<string> list9RF = new List<string>();
+        List<string> list9Rate = new List<string>();
+
+        List<string> list10CF = new List<string>();
+        List<string> list10RF = new List<string>();
+        List<string> list10Rate = new List<string>();
+
+        List<string> list11CF = new List<string>();
+        List<string> list11RF = new List<string>();
+        List<string> list11Rate = new List<string>();
+
+        List<string> list12CF = new List<string>();
+        List<string> list12RF = new List<string>();
+        List<string> list12Rate = new List<string>();
+
+        List<string> list13CF = new List<string>();
+        List<string> list13RF = new List<string>();
+        List<string> list13Rate = new List<string>();
+
+        List<string> list14CF = new List<string>();
+        List<string> list14RF = new List<string>();
+        List<string> list14Rate = new List<string>();
+
+        List<string> list15CF = new List<string>();
+        List<string> list15RF = new List<string>();
+        List<string> list15Rate = new List<string>();
+
         List<string> list16CF = new List<string>();
         List<string> list16RF = new List<string>();
         List<string> list16Rate = new List<string>();
@@ -34,6 +99,8 @@ namespace AI_Sports
             InitializeComponent();
             List<TrainingDeviceRecordEntity> list = trainingDeviceRecordService.GetRecordByIdAndTime("123456");
             List<CustomExpander> listCS = new List<CustomExpander>();
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string rootpath = path.Substring(0, path.LastIndexOf("bin"));
             foreach (var item in list)
             {
                 CustomExpander ce = new CustomExpander();
@@ -44,6 +111,9 @@ namespace AI_Sports
                         ce.Name = "腿部推蹬机";
                         ce.Image_address = "0.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list0CF.Add(item.Consequent_force.ToString());
+                        list0RF.Add(item.Reverse_force.ToString());
+                        list0Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         CHEST.DataContext = ce;
                         CHEST.Visibility = Visibility.Visible;
                         break;
@@ -51,6 +121,9 @@ namespace AI_Sports
                         ce.Name = "坐式背阔肌高拉机";
                         ce.Image_address = "1.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list1CF.Add(item.Consequent_force.ToString());
+                        list1RF.Add(item.Reverse_force.ToString());
+                        list1Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         LEG.DataContext = ce;
                         LEG.Visibility = Visibility.Visible;
                         break;
@@ -58,6 +131,9 @@ namespace AI_Sports
                         ce.Name = "三头肌训练机";
                         ce.Image_address = "2.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list2CF.Add(item.Consequent_force.ToString());
+                        list2RF.Add(item.Reverse_force.ToString());
+                        list2Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         LAT.DataContext = ce;
                         LAT.Visibility = Visibility.Visible;
                         break;
@@ -65,6 +141,9 @@ namespace AI_Sports
                         ce.Name = "腿部内弯机";
                         ce.Image_address = "3.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list3CF.Add(item.Consequent_force.ToString());
+                        list3RF.Add(item.Reverse_force.ToString());
+                        list3Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         SEATED.DataContext = ce;
                         SEATED.Visibility = Visibility.Visible;
                         break;
@@ -72,6 +151,9 @@ namespace AI_Sports
                         ce.Name = "腿部外弯机";
                         ce.Image_address = "4.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list4CF.Add(item.Consequent_force.ToString());
+                        list4RF.Add(item.Reverse_force.ToString());
+                        list4Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         ADDUCTOR.DataContext = ce;
                         ADDUCTOR.Visibility = Visibility.Visible;
                         break;
@@ -79,6 +161,9 @@ namespace AI_Sports
                         ce.Name = "蝴蝶机";
                         ce.Image_address = "5.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list5CF.Add(item.Consequent_force.ToString());
+                        list5RF.Add(item.Reverse_force.ToString());
+                        list5Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         BUTTERFLY.DataContext = ce;
                         BUTTERFLY.Visibility = Visibility.Visible;
                         break;
@@ -86,6 +171,9 @@ namespace AI_Sports
                         ce.Name = "反向蝴蝶机";
                         ce.Image_address = "6.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list6CF.Add(item.Consequent_force.ToString());
+                        list6RF.Add(item.Reverse_force.ToString());
+                        list6Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         BR.DataContext = ce;
                         BR.Visibility = Visibility.Visible;
                         break;
@@ -93,6 +181,9 @@ namespace AI_Sports
                         ce.Name = "坐式背部伸展机";
                         ce.Image_address = "7.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list7CF.Add(item.Consequent_force.ToString());
+                        list7RF.Add(item.Reverse_force.ToString());
+                        list7Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         BEB.DataContext = ce;
                         BEB.Visibility = Visibility.Visible;
                         break;
@@ -100,6 +191,9 @@ namespace AI_Sports
                         ce.Name = "躯干扭转组合";
                         ce.Image_address = "8.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list8CF.Add(item.Consequent_force.ToString());
+                        list8RF.Add(item.Reverse_force.ToString());
+                        list8Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         RC1.DataContext = ce;
                         RC1.Visibility = Visibility.Visible;
                         break;
@@ -107,6 +201,9 @@ namespace AI_Sports
                         ce.Name = "坐式腿伸展训练机";
                         ce.Image_address = "9.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list9CF.Add(item.Consequent_force.ToString());
+                        list9RF.Add(item.Reverse_force.ToString());
+                        list9Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S1.DataContext = ce;
                         S1.Visibility = Visibility.Visible;
                         break;
@@ -114,6 +211,9 @@ namespace AI_Sports
                         ce.Name = "坐式推胸机";
                         ce.Image_address = "10.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list10CF.Add(item.Consequent_force.ToString());
+                        list10RF.Add(item.Reverse_force.ToString());
+                        list10Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S2.DataContext = ce;
                         S2.Visibility = Visibility.Visible;
                         break;
@@ -121,6 +221,9 @@ namespace AI_Sports
                         ce.Name = "坐式划船机";
                         ce.Image_address = "11.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list11CF.Add(item.Consequent_force.ToString());
+                        list11RF.Add(item.Reverse_force.ToString());
+                        list11Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S3.DataContext = ce;
                         S3.Visibility = Visibility.Visible;
                         break;
@@ -128,6 +231,9 @@ namespace AI_Sports
                         ce.Name = "椭圆跑步机";
                         ce.Image_address = "12.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list12CF.Add(item.Consequent_force.ToString());
+                        list12RF.Add(item.Reverse_force.ToString());
+                        list12Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S4.DataContext = ce;
                         S4.Visibility = Visibility.Visible;
                         break;
@@ -135,6 +241,9 @@ namespace AI_Sports
                         ce.Name = "坐式屈腿训练机";
                         ce.Image_address = "13.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list13CF.Add(item.Consequent_force.ToString());
+                        list13RF.Add(item.Reverse_force.ToString());
+                        list13Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S5.DataContext = ce;
                         S5.Visibility = Visibility.Visible;
                         break;
@@ -142,6 +251,9 @@ namespace AI_Sports
                         ce.Name = "腹肌训练机";
                         ce.Image_address = "14.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list14CF.Add(item.Consequent_force.ToString());
+                        list14RF.Add(item.Reverse_force.ToString());
+                        list14Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S6.DataContext = ce;
                         S6.Visibility = Visibility.Visible;
                         break;
@@ -149,7 +261,12 @@ namespace AI_Sports
                         ce.Name = "坐式背部伸展机";
                         ce.Image_address = "15.png";
                         ce.Modified_time = item.Gmt_modified.ToString();
+                        list15CF.Add(item.Consequent_force.ToString());
+                        list15RF.Add(item.Reverse_force.ToString());
+                        list15Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S7.DataContext = ce;
+                        Aerobic15.Navigate(new Uri(rootpath + "AISports.Echarts/dist/Line.html"));
+                        Aerobic15.ObjectForScripting = new WebAerobic15();
                         S7.Visibility = Visibility.Visible;
                         break;
                     case "16":
@@ -160,22 +277,59 @@ namespace AI_Sports
                         list16RF.Add(item.Reverse_force.ToString());
                         list16Rate.Add((item.Consequent_force / item.Reverse_force).ToString());
                         S8.DataContext = ce;
+                        Aerobic16.Navigate(new Uri(rootpath + "AISports.Echarts/dist/Line.html"));
+                        Aerobic16.ObjectForScripting = new WebAerobic16();
                         S8.Visibility = Visibility.Visible;
                         break;
                 }
             }
-            Console.WriteLine(list16CF.Capacity);
-            Console.WriteLine(list16RF.Capacity);
-            Console.WriteLine(list16Rate.Capacity);
         }
 
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         [System.Runtime.InteropServices.ComVisible(true)]
-        public class WebAerobic
+        public class WebAerobic14
         {
             public string drawCF()
             {
-                return string.Join(",", new NolleWindow().list16CF);
+                string str14CF = string.Join(",", new NolleWindow().list14CF);
+                return str14CF;
+            }
+            public string drawRF()
+            {
+                string str14RF = string.Join(",", new NolleWindow().list14RF);
+                return str14RF;
+            }
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        [System.Runtime.InteropServices.ComVisible(true)]
+        public class WebAerobic15
+        {
+            public string drawCF()
+            {
+                string str15CF = string.Join(",", new NolleWindow().list15CF);
+                return str15CF;
+            }
+            public string drawRF()
+            {
+                string str15RF = string.Join(",", new NolleWindow().list15RF);
+                return str15RF;
+            }
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        [System.Runtime.InteropServices.ComVisible(true)]
+        public class WebAerobic16
+        {
+            public string drawCF()
+            {
+                string str16CF = string.Join(",", new NolleWindow().list16CF);
+                return str16CF;
+            }
+            public string drawRF()
+            {
+                string str16RF = string.Join(",", new NolleWindow().list16RF);
+                return str16RF;
             }
         }
 
