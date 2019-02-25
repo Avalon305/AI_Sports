c9f352e7f7eb1ff2e8315a764878532de5d2d0a3
mueller <321892803@qq.com>
2019-02-25 13:23:31 +0800

    整合语音

---
+++ AI_Sports/NolleTest.xaml.cs
@@ -0,0 +1,34 @@
+﻿using AI_Sports.AISports.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AI_Sports
+{
+    /// <summary>
+    /// NolleTest.xaml 的交互逻辑
+    /// </summary>
+    public partial class NolleTest : Window
+    {
+        public NolleTest()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SpeechUtil.read("hello 徐靖皓");
+
+        }
+    }
+}
