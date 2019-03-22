5f912f49ae0631d61efb25e08fee6e866e0c48fe
mueller <321892803@qq.com>
2019-03-22 22:20:08 +0800

    添加常量类

---
+++ AI_Sports/AISports.Constant/CommondConstant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI_Sports.AISports.Constant
+{
+    class CommondConstant
+    {
+        //发卡命令字
+        public static byte[] sendCard = { 0x01 };
+        //读卡命令字
+        public static byte[] readCard = { 0x02 };
+        //应答命令字
+        public static byte[] ResSendCard = { 0x81 };
+    }
+}
